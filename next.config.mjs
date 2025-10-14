798249d9045ddbb694925f1bf710b71bdd13ee6f
0SyedAli <sa8449006@gmail.com>
2025-10-15 03:12:42 +0500

    sharp error

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    unoptimized: true, // ✅ disables sharp requirement
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
