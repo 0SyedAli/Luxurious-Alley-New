3d77dec75934ed49d44d1bda818ae87e3781cb42
0SyedAli <sa8449006@gmail.com>
2025-10-15 03:00:15 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    unoptimized: true, // ✅ disables sharp requirement
+  },
+};
 
 export default nextConfig;
